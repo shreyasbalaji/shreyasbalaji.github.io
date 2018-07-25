53f9fb9495c6996470e5a27cd19abd0f0d0553d6
Shreyas Balaji <shreyasb99@gmail.com>
2018-07-25 01:20:21 -0400

    Styling changes

--- js/script.js
+++ js/script.js
@@ -19,26 +19,40 @@ function doResize() {
         $("div.container").css("width", pstringify(percentWidth));
         $("div.crescent").css("background-position", "" + (100 * (1.8 * percentWidth + 1) / 3 + 100 * 180 / window.innerWidth) + "% 0%");
     }
-    if (window.innerWidth <= 800 && window.innerHeight > 1000) {
-        if ($("div.home-quote-left").hasClass("one")) {
-            $("div.home-quote-left").removeClass("one");
-        }
-        if ($("div.home-quote-wrapper").hasClass("six")) {
-            $("div.home-quote-wrapper").removeClass("six");
-            $("div.home-quote-wrapper").addClass("seven");
-            $("div.home-quote-wrapper").addClass("less-left-margin");
-        }
+    if (window.innerWidth < 800) {
+        $(".less-left-margin").css("margin-left", "0").promise().done(function() {
+            var offset = $(".less-left-margin").offset()['left'];
+            $(".less-left-margin").css("margin-left", (-offset+24) + "px");
+        });
     }
     else {
-        if (!$("div.home-quote-left").hasClass("one")) {
-            $("div.home-quote-left").addClass("one");
-        }
-        if ($("div.home-quote-wrapper").hasClass("seven")) {
-            $("div.home-quote-wrapper").removeClass("seven");
-            $("div.home-quote-wrapper").addClass("six");
-            $("div.home-quote-wrapper").removeClass("less-left-margin");
-        }
+        $(".less-left-margin").css("margin-left", "");
     }
+
+
+    if (typed1 !== null && window.innerWidth < 450) {
+        typed1.stop();
+    }
+    // if (window.innerWidth <= 800 && window.innerHeight > 1000) {
+    //     if ($("div.home-quote-left").hasClass("one")) {
+    //         $("div.home-quote-left").removeClass("one");
+    //     }
+    //     if ($("div.home-quote-wrapper").hasClass("six")) {
+    //         $("div.home-quote-wrapper").removeClass("six");
+    //         $("div.home-quote-wrapper").addClass("seven");
+    //         $("div.home-quote-wrapper").addClass("less-left-margin");
+    //     }
+    // }
+    // else {
+    //     if (!$("div.home-quote-left").hasClass("one")) {
+    //         $("div.home-quote-left").addClass("one");
+    //     }
+    //     if ($("div.home-quote-wrapper").hasClass("seven")) {
+    //         $("div.home-quote-wrapper").removeClass("seven");
+    //         $("div.home-quote-wrapper").addClass("six");
+    //         $("div.home-quote-wrapper").removeClass("less-left-margin");
+    //     }
+    // }
 }
 
 function sizeProfile() {
@@ -54,10 +68,35 @@ function sizeProfile() {
 
 $(document).ready(function() {
     doResize();
-    sizeProfile();
-    // $("#profile").height(500);
     $(window).resize(function() {
         doResize();
-        sizeProfile();
     });
+});
+
+function getTypedConfig(stringsElement) {
+    return {
+      stringsElement: stringsElement,
+      smartBackspace: true,
+      backSpeed: 15,
+      backDelay: 500,
+      showCursor: false,
+      typeSpeed: 35
+    };
+}
+
+var typed1 = null;
+punslist = [
+    "You should <b>never</b> trust atoms. They make up <b>everything</b>.",
+    "Don't room with a <b>tennis player</b>. You'll have to <b>sleep</b> past the <b>racket</b>.",
+];
+
+$(document).ready(function() {
+    var pun = punslist[Math.floor(Math.random() * punslist.length)];
+    $("#pun").html(pun);
+});
+
+$(document).ready(function() {
+    if (window.innerWidth > 450) {
+        typed1 = new Typed('#typed1', getTypedConfig('#typed-strings1'));
+    }
 });
